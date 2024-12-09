3afb0526c0d06e53c1a5f32c44fa73a391d63ded
Justin Sun <justinsunyt@gmail.com>
2024-12-08 18:07:39 -0800

    explicit scrapybara_api_key

--- tests/custom/test_client.py
+++ tests/custom/test_client.py
@@ -1,8 +1,11 @@
 from scrapybara import Scrapybara
+import os
 
 
 def test_client() -> None:
-    client = Scrapybara()
+    if os.getenv("SCRAPYBARA_API_KEY") is None:
+        raise ValueError("SCRAPYBARA_API_KEY is not set")
+    client = Scrapybara(api_key=os.getenv("SCRAPYBARA_API_KEY"))
     instance = client.start()
     assert instance.id is not None
     screenshot_response = instance.screenshot()
