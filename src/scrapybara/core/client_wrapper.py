0716b7865f1770a0747c363268db602f688149ef
Justin Sun <justinsunyt@gmail.com>
2025-02-15 20:43:40 -0800

    fix: clone current_tools instead of ref

--- src/scrapybara/core/client_wrapper.py
+++ src/scrapybara/core/client_wrapper.py
@@ -18,7 +18,7 @@ class BaseClientWrapper:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "scrapybara",
-            "X-Fern-SDK-Version": "2.2.5",
+            "X-Fern-SDK-Version": "2.2.6",
         }
         headers["x-api-key"] = self.api_key
         return headers
