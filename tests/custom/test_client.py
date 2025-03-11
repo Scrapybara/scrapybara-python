b98e888c4ae609a7f8ba7f4a636e609e59017bc9
Cooper Miller <kcoopermiller9@gmail.com>
2025-03-11 10:59:01 -0500

    feat: oai

--- tests/custom/test_client.py
+++ tests/custom/test_client.py
@@ -5,9 +5,15 @@ import pytest
 
 from scrapybara.anthropic import (
     Anthropic,
-    UBUNTU_SYSTEM_PROMPT,
-    BROWSER_SYSTEM_PROMPT,
-    WINDOWS_SYSTEM_PROMPT,
+    UBUNTU_SYSTEM_PROMPT as UBUNTU_SYSTEM_PROMPT_ANTHROPIC,
+    BROWSER_SYSTEM_PROMPT as BROWSER_SYSTEM_PROMPT_ANTHROPIC,
+    WINDOWS_SYSTEM_PROMPT as WINDOWS_SYSTEM_PROMPT_ANTHROPIC,
+)
+from scrapybara.openai import (
+    OpenAI,
+    UBUNTU_SYSTEM_PROMPT as UBUNTU_SYSTEM_PROMPT_OPENAI,
+    BROWSER_SYSTEM_PROMPT as BROWSER_SYSTEM_PROMPT_OPENAI,
+    WINDOWS_SYSTEM_PROMPT as WINDOWS_SYSTEM_PROMPT_OPENAI,
 )
 from scrapybara.tools import BashTool, ComputerTool, EditTool
 
@@ -38,7 +44,40 @@ def test_ubuntu() -> None:
     assert cdp_url is not None
     response = client.act(
         model=Anthropic(),
-        system=UBUNTU_SYSTEM_PROMPT,
+        system=UBUNTU_SYSTEM_PROMPT_ANTHROPIC,
+        prompt="Go to the YC website and get the number of funded startups and combined valuation",
+        tools=[
+            ComputerTool(ubuntu_instance),
+            BashTool(ubuntu_instance),
+            EditTool(ubuntu_instance),
+        ],
+        schema=YCStats,
+        on_step=lambda step: print(step.text, step.tool_calls),
+    )
+    print(response.output)
+    assert response.output is not None
+    assert response.output.number_of_startups is not None
+    assert response.output.combined_valuation is not None
+    ubuntu_instance.browser.stop()
+    ubuntu_instance.stop()
+
+def test_ubuntu_openai() -> None:
+    _check_api_key()
+    client = Scrapybara()
+
+    ubuntu_instance = client.start_ubuntu()
+    print(ubuntu_instance.get_stream_url().stream_url)
+    assert ubuntu_instance.id is not None
+    instances = client.get_instances()
+    assert len(instances) > 0
+    screenshot_response = ubuntu_instance.screenshot()
+    assert screenshot_response.base_64_image is not None
+    ubuntu_instance.browser.start()
+    cdp_url = ubuntu_instance.browser.get_cdp_url()
+    assert cdp_url is not None
+    response = client.act(
+        model=OpenAI(),
+        system=UBUNTU_SYSTEM_PROMPT_OPENAI,
         prompt="Go to the YC website and get the number of funded startups and combined valuation",
         tools=[
             ComputerTool(ubuntu_instance),
@@ -69,7 +108,34 @@ def test_browser() -> None:
     assert cdp_url is not None
     response = client.act(
         model=Anthropic(),
-        system=BROWSER_SYSTEM_PROMPT,
+        system=BROWSER_SYSTEM_PROMPT_ANTHROPIC,
+        prompt="Go to the YC website and get the number of funded startups and combined valuation",
+        tools=[
+            ComputerTool(browser_instance),
+        ],
+        schema=YCStats,
+        on_step=lambda step: print(step.text, step.tool_calls),
+    )
+    print(response.output)
+    assert response.output is not None
+    assert response.output.number_of_startups is not None
+    assert response.output.combined_valuation is not None
+    browser_instance.stop()
+
+def test_browser_openai() -> None:
+    _check_api_key()
+    client = Scrapybara()
+
+    browser_instance = client.start_browser()
+    print(browser_instance.get_stream_url().stream_url)
+    assert browser_instance.id is not None
+    screenshot_response = browser_instance.screenshot()
+    assert screenshot_response.base_64_image is not None
+    cdp_url = browser_instance.get_cdp_url()
+    assert cdp_url is not None
+    response = client.act(
+        model=OpenAI(),
+        system=BROWSER_SYSTEM_PROMPT_OPENAI,
         prompt="Go to the YC website and get the number of funded startups and combined valuation",
         tools=[
             ComputerTool(browser_instance),
@@ -96,7 +162,7 @@ def test_windows() -> None:
     assert screenshot_response.base_64_image is not None
     response = client.act(
         model=Anthropic(),
-        system=WINDOWS_SYSTEM_PROMPT,
+        system=WINDOWS_SYSTEM_PROMPT_ANTHROPIC,
         prompt="Go to the YC website and get the number of funded startups and combined valuation",
         tools=[
             ComputerTool(windows_instance),
@@ -128,7 +194,7 @@ def test_ubuntu_thinking() -> None:
     assert cdp_url is not None
     response = client.act(
         model=Anthropic(name="claude-3-7-sonnet-20250219-thinking"),
-        system=UBUNTU_SYSTEM_PROMPT,
+        system=UBUNTU_SYSTEM_PROMPT_ANTHROPIC,
         prompt="Go to the YC website and get the number of funded startups and combined valuation",
         tools=[
             ComputerTool(ubuntu_instance),
@@ -160,7 +226,7 @@ def test_browser_thinking() -> None:
     assert cdp_url is not None
     response = client.act(
         model=Anthropic(name="claude-3-7-sonnet-20250219-thinking"),
-        system=BROWSER_SYSTEM_PROMPT,
+        system=BROWSER_SYSTEM_PROMPT_ANTHROPIC,
         prompt="Go to the YC website and get the number of funded startups and combined valuation",
         tools=[
             ComputerTool(browser_instance),
@@ -178,6 +244,8 @@ def test_browser_thinking() -> None:
 if __name__ == "__main__":
     test_ubuntu()
     test_browser()
+    test_ubuntu_openai()
+    test_browser_openai()
     # test_ubuntu_thinking()
     # test_browser_thinking()
     # test_windows()
