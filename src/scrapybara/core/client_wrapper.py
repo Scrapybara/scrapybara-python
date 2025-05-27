34235981651eb3b66af1f8b16d7ae9d762edbf57
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-05-27 22:13:27 +0000

    SDK regeneration

--- src/scrapybara/core/client_wrapper.py
+++ src/scrapybara/core/client_wrapper.py
@@ -16,7 +16,7 @@ class BaseClientWrapper:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "scrapybara",
-            "X-Fern-SDK-Version": "2.5.1",
+            "X-Fern-SDK-Version": "2.5.2",
         }
         headers["x-api-key"] = self.api_key
         return headers
